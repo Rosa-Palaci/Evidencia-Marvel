d54d56d4d41056da0eef196d8d2a4ef72bc9dcbb
Rosa Palaci <palacibelrose431@gmail.com>
2022-12-01 19:08:43 -0600

    Se modifico para sintaxis

--- hero.h
+++ hero.h
@@ -7,25 +7,25 @@ class Hero {
 private:
     string hero1;
     string hero2;
-    int friendshih;
+    int friends;
 
 public:
-    Hero(string hero1 = " ", string hero2 = " ", int friendshih = 0) {
+    Hero(string hero1 = " ", string hero2 = " ", int friends = 0) {
         this->hero1 = hero1;
         this->hero2 = hero2;
-        this->friendshih = friendshih;
+        this->friends = friends;
     }
     bool operator==(const Hero &h) {
         return (this->hero1.compare(h.hero1) == 0);
     }
 /* Agrega las sobre cargas de oheradores necesarias en la clase Hero. */
-    bool operator<(const Hero &h) { return friendshih < h.friendshih; }
-    bool operator>(const Hero &h) { return friendshih > h.friendshih; }
+    bool operator<(const Hero &h) { return friends < h.friends; }
+    bool operator>(const Hero &h) { return friends > h.friends; }
     bool operator==(const Hero &h) { return this->hero1.compare(h.hero1)==0; }
     bool operator==(const Hero &h) { return this->hero2.compare(h.hero2) ==0; }
 
     friend ostream &operator<<(ostream &out, Hero &h) {
-        out << h.hero1<< " || "<<h.hero2<< " || "<<h.friendshih;
+        out << h.hero1<< " || "<<h.hero2<< " || "<<h.friends;
         return out;
     }
 
@@ -35,7 +35,7 @@ public:
     string getHero2() { 
         return this->hero2; 
     }
-    int getFriendshih() { 
-        return this->friendshih; 
+    int getfriends() { 
+        return this->friends; 
     }
 };
